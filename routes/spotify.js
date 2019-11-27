b618ec36516931726cc64eb3ab19f25dc33deac1
Sean Johnson <sean.johnson.ca@gmail.com>
2019-11-27 12:59:51 -0800

    tracks

--- routes/spotify.js
+++ routes/spotify.js
@@ -5,6 +5,8 @@ const SpotifyApi = require("../lib/spotify-api");
 
 require("dotenv").config();
 
+const DEF_PERSONALIZATION_LIMIT = 20
+
 const spotifyApi = new SpotifyApi().getInstance();
 
 const spotifyAuth = passport.authenticate("spotify", {
@@ -36,7 +38,7 @@ router.get("/auth/callback", spotifyAuth, async (req, res) => {
 router.get("/api/my-top-artists", addSocketIdtoSession, async (req, res) => {
   const io = req.app.get("io");
 
-  const limit = req.query.limit ? parseInt(req.query.limit) : 20;
+  const limit = req.query.limit ? parseInt(req.query.limit) : DEF_PERSONALIZATION_LIMIT;
   const n = req.query.n ? req.query.n : limit;
   const promises = [];
 
@@ -65,18 +67,38 @@ router.get("/api/my-top-artists", addSocketIdtoSession, async (req, res) => {
     });
 });
 
-router.get("/api/my-top-tracks", async (req, res) => {
-  try {
+// Fetches `n` tracks in accordance with limit, emitting an aggregate result
+// Note: Spotify does not currently provide >50 results, but may change this in the future.
+router.get("/api/my-top-tracks", addSocketIdtoSession, async (req, res) => {
+  const io = req.app.get("io");
+
+  const limit = req.query.limit ? parseInt(req.query.limit) : DEF_PERSONALIZATION_LIMIT;
+  const n = req.query.n ? req.query.n : limit;
+  const promises = [];
+
+  for (let i = 0; i <= n; i += limit) {
     const options = {
       time_range: req.query.time_range,
       limit: req.query.limit,
-      offset: req.query.offset
+      offset: i
     };
-    const result = await spotifyApi.getMyTopTracks(options);
-    res.status(200).send(result.body);
-  } catch (err) {
-    res.status(400).send(err);
+
+    promises.push(spotifyApi.getMyTopTracks(options));
   }
+
+  await Promise.all(promises)
+    .then((results) => {
+      const result = {
+        items: results.map(r => r.body.items).flat(),
+        range: req.query.time_range
+      };
+
+      io.in(req.session.socketId).emit('topTracks', result);
+    })
+    .catch((err) => {
+      console.log(err)
+      io.in(req.session.socketId).emit('error', err);
+    });
 });
 
 router.get("/api/track-features", async (req, res) => {
