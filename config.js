a55fbf810b78938f3caaeeb04f52821e7bfe6917
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-06 13:13:54 -0800

    adjust deploy settings

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? /.*topspotify.netlify.com/
+  ? /.*topspot.netlify.com/
   : ['http://localhost:3001/', /.*3001.*/];
 
 exports.SPOTIFY_CONFIG = {
   clientID: process.env.SPOTIFY_API_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  callbackURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://top-spotify.herokuapp.com/auth/callback'
-      : process.env.CALLBACK_URL,
+  callbackURL: process.env.CALLBACK_URL,
 };
