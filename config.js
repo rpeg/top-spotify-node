4423d3a6b4b2899305bfd7dd3a5603e68d0ebeb3
Sean Johnson <sean.johnson.ca@gmail.com>
2020-04-25 10:08:46 -0700

    fix netlify url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? /.*topspot.netlify.com/
+  ? /.*topspot.netlify.app/
   : ['http://localhost:3001/', /.*3001.*/];
 
 exports.SPOTIFY_CONFIG = {
